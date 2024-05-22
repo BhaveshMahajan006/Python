1d4ca238c863ec7c6635019905a325abccc8d82f
Bhavesh <bhaveshmahjen@gmail.com>
2024-05-22 19:46:40 +0530

    calculator added

---
+++ _004_Calculator.py
@@ -0,0 +1,10 @@
+a=int(input("Enter First number: "))
+b=int(input("Enter Second number: "))
+
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+modulo_division=a%b
+
+print(addition, "\n", subtraction,"\n", multiplication,"\n", division,"\n", modulo_division)
